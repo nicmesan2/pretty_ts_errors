42de737d440426a73a088b5785df88eee4b5430b
GitHub Action <action@github.com>
2023-04-18 18:19:11 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "pretty_ts_errors"
--- src/main/kotlin/com/github/nicmesan2/prettytserrors/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nicmesan2/prettytserrors/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nicmesan2.prettytserrors.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nicmesan2.prettytserrors.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
